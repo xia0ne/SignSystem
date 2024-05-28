aaa54b4ddd4f771d3d479bbfdadac42d1670bc01
xia0ne <1@myyrh.com>
2024-05-28 15:19:02 +0800

    init: init this project

---
+++ src/main/java/yrh/signsystem/SignSystemApplication.java
@@ -0,0 +1,13 @@
+package yrh.signsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yrh/signsystem/SignSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package yrh.signsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
